8cef40a0659fd9ab0718636237c7a07a97db612d
wangbl <wangbl@lantunet.com>
2019-04-08 15:38:32 +0800

    update:增加esc关闭菜单功能

--- clickoutside.js
+++ clickoutside.js
@@ -1,7 +1,8 @@
 Vue.directive("clickOutside", {
     bind : function(el, binding, vnode){
         function documentHandler(e){
-            if(el.contains(e.target)){
+            console.log(e);
+            if(e.type === "click" && el.contains(e.target)){
                 return false;
             }
             if(binding.expression){
@@ -10,9 +11,11 @@ Vue.directive("clickOutside", {
         }
         el._vueClickOutside_ = documentHandler;
         document.addEventListener("click", documentHandler);
+        document.addEventListener("keydown", documentHandler);
     },
     unbind : function(el, binding){
         document.removeEventListener("click", el._vueClickOutside_);
+        document.removeEventListener("keydown", el._vueClickOutside_);
         delete el._vueClickOutside_;
     }
 });
